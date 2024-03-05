ea79a559d8463891067bee87651a84765c967160
Aguil3ra <mariajoseaguileraaguirre71@gmail.com>
2024-03-05 19:53:05 -0400

    primer repositorio

---
+++ contador de palbras.php
@@ -0,0 +1,24 @@
+<?php
+function contpalabras();
+{
+    $cont = 0;    
+    $cad = strtolower ($cad "hola mundo");
+    $lg = strlen($cad);
+    {
+        for [$pos=0; $pos<$lg -1; $pos ++];
+        {
+            $pal = $cad[$pos];
+            while
+            {
+                $pal = ' ';
+                $cont= $cont++
+            }
+            
+        }
+        return $cont;
+    }
+ 
+}
+$res= contpalabras();
+echo "en la oracion existen" ,$cont, "palabras"
+?>
