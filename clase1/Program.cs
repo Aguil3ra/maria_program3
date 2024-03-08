b2101443b6e72df58a175e156a80a7baf5e654be
Aguil3ra <mariajoseaguileraaguirre71@gmail.com>
2024-03-07 21:55:27 -0400

    clase, jueves

--- clase1/Program.cs
+++ clase1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("clase dl dia de hoy  cambio  extra");
+﻿using System;
+using System.Diagnostics;
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("porfavor ingrese su numero");
+            // int nota = int.Parse(Console.ReadLine());
+
+            // EvualuarNota(nota);
+
+            rotar2();
+            
+        }
+
+        static void EvualuarNota(int nota)
+        {
+            if (nota>50)
+            {
+                Console.WriteLine("aprobado");
+            }
+            else
+            {
+               Console.WriteLine("reprobado"); 
+            }
+        } 
+        static void rotar()
+        {
+            string numero = Console.ReadLine();
+            char ultimo = numero[numero.Length -1];
+            string resto =numero.Substring(0, numero.Length -1);
+            string resultado = ultimo + resto;
+            Console.WriteLine(resultado);
+        }
+
+        static void rotar2()
+        {
+            string numero = Console.ReadLine();
+            char primero = numero[0];
+            char segundo = numero[1];
+            // char ultimo = numero[numero.Length -1];
+            string resto =numero.Substring(2, numero.Length -2);
+            string resultado = resto + primero + segundo;
+            Console.WriteLine(resultado);
+        }
+    }
+}
